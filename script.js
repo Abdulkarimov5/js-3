90b354f5bbc79d1d81b376ce050a92e09c69064f
Абдулкаримов Олег <279015582@TOP.ACADEMY>
2024-10-03 20:47:00 +0300

    Объекты

---
+++ script.js
@@ -0,0 +1,31 @@
+/* let user = {     // объект
+    name: "John",  // под ключом "name" хранится значение "John"
+    age: 30        // под ключом "age" хранится значение 30
+};
+
+// получаем свойства объекта:
+alert( user.name ); // John
+alert( user.age ); // 30 */
+
+/* const user = {
+    name: "John",
+    surname: "Smith",
+}
+user.name = "Pete";
+delete user.name;
+
+console.log(user) */
+
+/* let sum = 0
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+for (const key in salaries) {
+    console.log(salaries[key])
+    sum += salaries[key]
+}
+
+console.log("Общая сумма:", sum) */
